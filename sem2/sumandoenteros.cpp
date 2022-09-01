aa969f64c564365fb6e3596d8dfed5ea9b205717
rodry1603 <rodrigo.gomez.sanroman@ucsp.edu.pe>
2022-09-01 08:23:14 -0500

    agregar sumando enteros

---
+++ sem2/sumandoenteros.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    a=20;
+
+    int b;
+    b=10;
+
+    int suma = a + b;
+
+    cout << "la suma es :" << suma << endl;
+
+    return 0;
+}
