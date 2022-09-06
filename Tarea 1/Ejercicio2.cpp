81aa6fe6d2ce06b1e488084b510eb66946902dd0
rodry1603 <112088811+rodry1603@users.noreply.github.com>
2022-09-06 04:09:51 -0500

    Add files via upload

---
+++ Tarea 1/Ejercicio2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"  CCC  +      +"<< endl;
+    cout<<" C     +      +"<<  endl;
+    cout<<"C    +++++  +++++"<< endl;
+    cout<<" C     +      +"<< endl;
+    cout<<"  CCC  +      +"<< endl;
+
+    return 0;
+
+}
