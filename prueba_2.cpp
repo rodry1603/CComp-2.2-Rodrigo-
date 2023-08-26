8b8dcb36a2b9e7bab72764b4c5a304a66f2810af
rodry1603 <112088811+rodry1603@users.noreply.github.com>
2023-08-26 09:54:42 -0500

    Add files via upload

---
+++ prueba_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int contador(int x){
+	
+	int lg = log10(x);
+	
+	char unidades[10][20] = { "","uno","dos", "tres","cuatro","cinco","seis","siete","ocho","nueve"};
+	char decenas[10][20] = { "","diez","veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta","ochenta","noventa" };
+	char centenas[10][20] = { "","cien","doscientos","trescientos","cuatrocientos","quinientos","seiscientos","setecientos","ochocientos","novecientos" };
+	char unidad_mil[10][20] = { "","mil","dosmil","tresmil","cuatromil","cincomil","seismil","sietemil","ochomil","nuevemil" };
+	char decena_mil[10][20]
+	char centena_mil[10][20]
+	char millon
+}
