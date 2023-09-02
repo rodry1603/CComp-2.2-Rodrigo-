ff4721f2887f748ed35da995ed8aaef5d4100067
rodry1603 <112088811+rodry1603@users.noreply.github.com>
2023-09-02 01:19:48 -0500

    Add files via upload

--- trabajo_1_2.cpp
+++ trabajo_1_2.cpp
@@ -19,35 +19,72 @@ string convierte_digito(unsigned long long n, string sufijo){
     if(n>29&&(n%10)==0){
         return y[n/10]+ sufijo;
     }else if(n>29&&(n%10)!=0){
-        return y[n/10]+ vacio + "y" + vacio + x[n%10]+ sufijo;
+        return y[n/10]+ vacio + "y" + vacio + x[n%10]+vacio+ sufijo;
     }else{
         return x[n] + sufijo;
     }
+}
+
+string centenas(unsigned long long n){
+    string x;
+    switch ((n/100)%10)
+    {
+    case 1:
+        if((n%100)==0){
+            x="cien";
+        }else if((n%100)!=0){
+            x= "ciento" + vacio;
+        }
+        break;
+    case 5:
+        x="quinientos"+ vacio;
+        break;
+    case 7:
+        x="setecientos"+ vacio;
+        break;
+    case 9:
+        x="novecientos"+ vacio;
+        break;
+    default:
+        x=convierte_digito(((n/100)%10),"cientos") + vacio ;
+        break;
+    }
+
+    return x;
 }
 
 string convertir(unsigned long long n){
 
     string res;
     res =(convierte_digito((n%100),""));
-    switch ((n/100)%10)
-    {
-    case 5:
-        res="quinientos"+ vacio + res;
-        break;
-    case 7:
-        res="setecientos"+ vacio + res;
-    case 9:
-        res="novecientos"+ vacio + res;
-    default:
-        res=convierte_digito(((n/100)%10),"cientos") + vacio + res;
-        break;
+    if(((n/100)%10)){
+        res=centenas(n) + res;
     }
-    res=convierte_digito(((n/1000)%100),"mil") + vacio + res;
+
+    if(((n/1000)%100)==1){
+        res="mil"+ vacio + res;
+    }else{
+        res=convierte_digito(((n/1000)%100),"mil") + vacio + res;
+    } 
+    
+    if((n/1000)>=100){
+        string x=convierte_digito(((n/1000)%100),"");
+        res=centenas(n/1000)+x+vacio+"mil"+vacio+res;
+    }
+
+    if((n/1000000)==1){
+        res="un millon";
+    }
+
     return res;
+
 
 
 }
 
 int main(){
-    cout<< convertir(201);
+    int x;
+    cout<<"ingrese un numero entre 1 a 1000000"<<endl;
+    cin>>x;
+    cout<< convertir(x);
 }
